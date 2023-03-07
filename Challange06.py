d19909de6899c1c11217e3c943088c9b3ba5146c
Hugo Rebelo <122793759+birlzhimself@users.noreply.github.com>
2023-03-07 20:05:36 +0000

    Update Challange06.py

--- Challange06.py
+++ Challange06.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
-#Script Ops Challenge: Class 06 - Bash in Python #Purpose Create a python script that executes bash commands, saves them as variables and outputs them to the terminal. #Why Learn how to import a module, save bash commands output as a variable and print it to the terminal.
+#Script Ops Challenge: Class 06 - Bash in Python 
+#Purpose Create a python script that executes bash commands, saves them as variables and outputs them to the terminal. 
+#Why Learn how to import a module, save bash commands output as a variable and print it to the terminal.
 
 import os
 
@@ -10,7 +12,9 @@ ip_output = os.popen(‘ip a’).read()
 
 lshw_output = os.popen(‘lshw -short’).read()
 
-print(“Output of whoami command:\n”, whoami_output) print(“Output of ip a command:\n”, ip_output) print(“Output of lshw -short command:\n”, lshw_output)
+print(“Output of whoami command:\n”, whoami_output) 
+print(“Output of ip a command:\n”, ip_output) 
+print(“Output of lshw -short command:\n”, lshw_output)
 
 import subprocess
 
@@ -20,4 +24,6 @@ ip_output = subprocess.run([‘ip’, ‘a’], capture_output=True, text=True).
 
 lshw_output = subprocess.run([‘lshw’, ‘-short’], capture_output=True, text=True).stdout
 
-print(“Output of whoami command:\n”, whoami_output) print(“Output of ip a command:\n”, ip_output) print(“Output of lshw -short command:\n”, lshw_output)
+print(“Output of whoami command:\n”, whoami_output) 
+print(“Output of ip a command:\n”, ip_output) 
+print(“Output of lshw -short command:\n”, lshw_output)
