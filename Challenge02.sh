e14d509ab3e83bfbc28efc128cdedb230ffbdff9
Hugo Rebelo <122793759+birlzhimself@users.noreply.github.com>
2023-03-07 21:47:25 +0000

    Update Challenge02.sh

--- Challenge02.sh
+++ Challenge02.sh
@@ -1,8 +1,8 @@
 #!/bin/bash/
 
-#Script Ops Challenge: Class 02 – Append; Date and Time
-#Purpose This script copies the /var/log/journal to the current working directory and prints the date
-#Why Learn to write and complete Bash Files
+#Script:     Ops Challenge: Class 02 – Append; Date and Time
+#Purpose:    This script copies the /var/log/journal to the current working directory and prints the date
+#Why:        Learn to write and complete Bash Files
 
 #Create Variable
 timestamp=$(date “+%Y-%m-%d_%H:%M:%S”)
