06a299192629ac1a79a580c331f1d56440a5dec5
Hugo Rebelo <122793759+birlzhimself@users.noreply.github.com>
2023-03-16 19:44:38 +0000

    Update Challange10.py

--- Challange10.py
+++ Challange10.py
@@ -20,23 +20,3 @@ with open("example.txt", "r") as f:
 
 # Delete the file
 os.remove("example.txt")
-
-
-import json
-
-# Open the pfSense configuration file
-with open("config.xml", "r") as f:
-    config = f.read()
-
-# Modify a value in the configuration file
-config_json = json.loads(config)
-config_json["some_setting"] = "new_value"
-modified_config = json.dumps(config_json)
-
-# Save the modified configuration to a new file
-with open("modified_config.xml", "w") as f:
-    f.write(modified_config)
-
-# Import the modified configuration to pfSense
-# (this assumes you have a way to import configurations to pfSense programmatically)
-import_config("modified_config.xml")
